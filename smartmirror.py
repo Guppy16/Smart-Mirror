d1ce1cb5b65b9392472ed7aab53f3ee98b81f46a
Guppy16 <guptaalgebra@gmail.com>
2020-06-14 02:34:33 +0100

    Edit main file

--- smartmirror.py
+++ smartmirror.py
@@ -17,14 +17,14 @@ from contextlib import contextmanager
 LOCALE_LOCK = threading.Lock()
 
 ui_locale = '' # e.g. 'fr_FR' fro French, '' as default
-time_format = 12 # 12 or 24
+time_format = 24 # 12 or 24
 date_format = "%b %d, %Y" # check python doc for strftime() for options
-news_country_code = 'us'
-weather_api_token = '<TOKEN>' # create account at https://darksky.net/dev/
+news_country_code = 'uk'
+weather_api_token = '19990d6eac810c0fa88f06a5a1688b91' # create account at https:/darksky.net/dev/
 weather_lang = 'en' # see https://darksky.net/dev/docs/forecast for full list of language parameters values
 weather_unit = 'us' # see https://darksky.net/dev/docs/forecast for full list of unit parameters values
-latitude = None # Set this if IP location lookup does not work for you (must be a string)
-longitude = None # Set this if IP location lookup does not work for you (must be a string)
+latitude = '51.4' # Set this if IP location lookup does not work for you (must be a string)
+longitude = '-0.07' # Set this if IP location lookup does not work for you (must be a string)
 xlarge_text_size = 94
 large_text_size = 48
 medium_text_size = 28
